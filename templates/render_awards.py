75702fa11b927a830519ecf6e159f52ac3bf686d
VKK5 <64189238+VKK5@users.noreply.github.com>
2021-07-02 12:04:08 +0530

    Updated render_records.py

--- templates/render_awards.py
+++ templates/render_awards.py
@@ -15,56 +15,41 @@ import ast
 
 def getData(row):
 	# Translation and Transliteration
-	try:
-		player_name = transliterate_text(row.Player_Name.values[0].strip('[]'), lang_code='te')
-	except:
-		player_name = transliterate(row.Player_Name.values[0].lower(), sanscript.HK, sanscript.TELUGU)
-
-
-	all_records = str(row.records_telugu.values[0])
-	test_records = str(row.test_records_telugu.values[0])
-	odi_records = str(row.odi_records_telugu.values[0])
-	t20i_records = str(row.t20i_records_telugu.values[0])
-	awards = str(row.awards_telugu.values[0])
+	player_name = row.Player_Name_Telugu.values[0]
+	all_records = row.records_telugu.values[0]
+	test_records = row.test_records_telugu.values[0]
+	odi_records = row.odi_records_telugu.values[0]
+	t20i_records = row.t20i_records_telugu.values[0]
 	gender = row.Gender.values[0]
 	references = ast.literal_eval(row.References.values[0])
 
 
-	if(all_records !='nan'):
+	if(all_records !='[]'):
 		try:
 			all_records = ast.literal_eval(all_records)
 		except:
 			all_records = ast.literal_eval(row.records.values[0])
 
 	
-	if(test_records != 'nan'):
+	if(test_records != '[]'):
 		try:
 			test_records = ast.literal_eval(test_records)
 		except:
 			test_records = ast.literal_eval(row.test_records.values[0])
 
 	
-	if(odi_records != 'nan'):
+	if(odi_records != '[]'):
 		try:
 			odi_records = ast.literal_eval(odi_records)
 		except:
 			odi_records = ast.literal_eval(row.odi_records.values[0])
 
 	
-	if(t20i_records != 'nan'):
+	if(t20i_records != '[]'):
 		try:
 			t20i_records = ast.literal_eval(t20i_records)
 		except:
 			t20i_records = ast.literal_eval(row.t20i_records.values[0])
-
-
-	if(awards != 'nan'):
-		try:
-			awards = ast.literal_eval(awards)
-		except:
-			awards = ast.literal_eval(row.awards.values[0])
-
-
 
 
 	# Data dictionary 
@@ -75,7 +60,6 @@ def getData(row):
 		'test_records': test_records, 
 		'odi_records': odi_records,
 		't20i_records': t20i_records,
-		'awards': awards,
 		'gender': gender,
 		'references': references
 	  }
@@ -87,12 +71,11 @@ def getData(row):
 def getData2(row):
 	data = {
 		
-		'player_name':row.Player_Name.values[0],
+		'player_name':row.Player_Name_Telugu.values[0],
 		'all_records': ast.literal_eval(row.records.values[0]), 
 		'test_records': ast.literal_eval(row.test_records.values[0]), 
 		'odi_records': ast.literal_eval(row.odi_records.values[0]),
 		't20i_records': ast.literal_eval(row.t20i_records.values[0]),
-		'awards':ast.literal_eval(row.awards.values[0]),
 		'gender': row.Gender.values[0],
 		'references': ast.literal_eval(row.References.values[0])
 	  }
@@ -114,7 +97,7 @@ def main():
 	env = Environment(loader=file_loader)
 	template = env.get_template('records.j2')
 
-	cricketDF =pickle.load(open('./data/cricket_players.pkl', 'rb'))
+	cricketDF =pickle.load(open('./data/final_cricket_players_translated_dataset_with_images.pkl', 'rb'))
 
 
 	func_dict = {
@@ -122,7 +105,10 @@ def main():
 	}
 	template.globals.update(func_dict)
 	
-	#cricketDF.rename(columns={'Records' : 'records'}, inplace=True)
+	cricketDF.rename(columns={'Records' : 'records'}, inplace=True)
+	cricketDF.rename(columns={'Test Records' : 'test_records'}, inplace=True)
+	cricketDF.rename(columns={'ODI Records' : 'odi_records'}, inplace=True)
+	cricketDF.rename(columns={'T20I Records' : 't20i_records'}, inplace=True)
 	
 	# Initiate the file object
 	fobj = open('recordsAwards.xml', 'w', encoding='utf-8')
@@ -133,8 +119,7 @@ def main():
 	#row = cricketDF.loc[cricketDF['Cricinfo_id'] == 54950]
 
 	text = template.render(getData(row))
-	player_name = row.Player_Name.values[0]
-	all_records = row.records.values[0]	
+	player_name = row.Player_Name_Telugu.values[0]
 	
 	writePage(player_name, text, fobj)
 	
